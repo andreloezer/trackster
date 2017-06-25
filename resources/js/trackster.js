5d29ab6aa152aa6a379f9f82b00381cdc321ab98
andreloezer <andrecesarloezer@gmail.com>
2017-06-25 21:08:47 +0000

    Trackster.renderTracks

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -6,14 +6,44 @@ $(document).ready(function() {
   $('.btn').click(function() {
     Trackster.searchTracksByTitle($('input').val());
   });
-});
+  document.onkeypress = function (e) {
+    switch (e.code) {
+      case "NumpadEnter":
+      case "Enter":
+        Trackster.searchTracksByTitle($('input').val());
+        break;
+      }
+}});
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  $('#song-section').empty();
+  for (var i = 0; i < tracks.length; i++) {
+    var mediumAlbumArt = tracks[i].image[1]["#text"];
+    console.log('Name: ' + tracks[i].name);
+    console.log('Artist: ' + tracks[i].artist);
+    console.log('Listeners: ' + tracks[i].listeners);
+    console.log('URL: ' + tracks[i].url);
+    console.log(mediumAlbumArt);
+    console.log('**********************');
 
+
+    var songRow =
+      '<div class="row">' +
+        '<ul class="container" id="song-info">' +
+          '<li class="col-xs-1"><a target="_blank" href=' + tracks[i].url + '><i class="fa fa-play-circle-o fa-2x"></i></a></li>' +
+          '<li class="col-xs-5">' + (i + 1) + '. ' + tracks[i].name + '</li>' +
+          '<li class="col-xs-2">' + tracks[i].artist + '</li>' +
+          '<li class="col-xs-2"><img src=' + mediumAlbumArt + ' /></li>' +
+          '<li class="col-xs-2">' + tracks[i].listeners + '</li>' +
+        '</ul>' +
+      '</div>';
+
+    $('#song-section').append(songRow);
+  }
 };
 
 /*
@@ -23,7 +53,8 @@ Trackster.renderTracks = function(tracks) {
 Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
-  }).done(function() {
-    console.log($(this));
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
   });
 };
