c4bab2ff508f2714fc6c2feca391d3559987df4e
andreloezer <andrecesarloezer@gmail.com>
2017-06-27 05:29:05 +0000

    Sorting added and minor improvements

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -3,46 +3,62 @@ var Trackster = {};
 var API_KEY = '86eb9d030d23409b7b148615386b8918';
 
 $(document).ready(function() {
+
   $('.btn').click(function() {
     Trackster.searchTracksByTitle($('input').val());
   });
-  document.onkeypress = function (e) {
-    switch (e.code) {
-      case "NumpadEnter":
-      case "Enter":
-        Trackster.searchTracksByTitle($('input').val());
-        break;
-      }
-}});
+
+  $('.track-header').click(function() {
+    Trackster.searchTracksByTitle($('input').val(), 'name')
+  })
+
+  $('.artist-header').click(function() {
+    Trackster.searchTracksByTitle($('input').val(), 'artist')
+  })
+
+  $('.album-art').click(function() {
+    Trackster.searchTracksByTitle($('input').val(), 'artist')
+  })
+
+  $('.listeners-header').click(function() {
+    Trackster.searchTracksByTitle($('input').val(), 'listeners')
+  })
+
+  $(document).keydown(function(e) {
+    if (e.which == 13) {
+      Trackster.searchTracksByTitle($('input').val());
+    }
+  });
+});
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-  $('#song-section').empty();
+
+  $('#tracklist-section').empty();
+  $('#tracklist-head').css('display', 'block');
+
   for (var i = 0; i < tracks.length; i++) {
+
     var mediumAlbumArt = tracks[i].image[1]["#text"];
-    console.log('Name: ' + tracks[i].name);
-    console.log('Artist: ' + tracks[i].artist);
-    console.log('Listeners: ' + tracks[i].listeners);
-    console.log('URL: ' + tracks[i].url);
-    console.log(mediumAlbumArt);
-    console.log('**********************');
+    var listenersNumber = numeral(tracks[i].listeners).format('0,0');
+    var artistData = tracks[i].artist.split(" ").join("");
 
+    var trackList =
 
-    var songRow =
       '<div class="row">' +
-        '<ul class="container" id="song-info">' +
+        '<ul class="container" id="track-info">' +
           '<li class="col-xs-1"><a target="_blank" href=' + tracks[i].url + '><i class="fa fa-play-circle-o fa-2x"></i></a></li>' +
           '<li class="col-xs-5">' + (i + 1) + '. ' + tracks[i].name + '</li>' +
-          '<li class="col-xs-2">' + tracks[i].artist + '</li>' +
+          '<li class="col-xs-2 artist" data-artist=' + artistData +'>' + tracks[i].artist + '</li>' +
           '<li class="col-xs-2"><img src=' + mediumAlbumArt + ' /></li>' +
-          '<li class="col-xs-2">' + tracks[i].listeners + '</li>' +
+          '<li class="col-xs-2">' + listenersNumber + '</li>' +
         '</ul>' +
       '</div>';
 
-    $('#song-section').append(songRow);
+    $('#tracklist-section').append(trackList);
   }
 };
 
@@ -50,11 +66,35 @@ Trackster.renderTracks = function(tracks) {
   Given a search term as a string, query the LastFM API.
   Render the tracks given in the API query response.
 */
-Trackster.searchTracksByTitle = function(title) {
+Trackster.searchTracksByTitle = function(title, sort) {
+
+  $('h1').addClass('app-title');
   $.ajax({
     url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
     success: function(response) {
-      Trackster.renderTracks(response.results.trackmatches.track);
+
+      var responseTrack = response.results.trackmatches.track;
+
+      if (sort != undefined) {  // called from the list header
+        var sorted = responseTrack.sort(function(x, y) {
+          if (isNaN(x[sort])) { // sorting for #Song or Artist
+            if (x[sort].toUpperCase() > y[sort].toUpperCase()) {
+              return 1;
+            }
+            if (x[sort].toUpperCase() < y[sort].toUpperCase()) {
+              return -1;
+            }
+            return 0;
+          } else { // sorting for Listeners
+            return y[sort] - x[sort];
+          }
+        })
+      } else {
+        var sorted = responseTrack;
+      }
+
+      Trackster.renderTracks(sorted);
+      $('h1').removeClass('app-title'); // remove animation after the list is rendered
     }
   });
 };
