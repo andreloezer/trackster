05c91db334cdde553e1cee615ee0b9d7e8ad269d
andreloezer <andrecesarloezer@gmail.com>
2017-06-28 02:30:36 +0000

    Sorting no longer call .ajax

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,7 +1,8 @@
-var Trackster = {};
-// const API_KEY = '86eb9d030d23409b7b148615386b8918';
-var API_KEY = '86eb9d030d23409b7b148615386b8918';
+// Variables
 
+var Trackster = {}; // object for the methods
+var responseTrack = {}; // stores the json
+var API_KEY = '86eb9d030d23409b7b148615386b8918'; // personal key to access 'last.fm' API
 var sortCount = { // keep track of sorting
   name: 1,
   artist: 1,
@@ -42,36 +43,30 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-
   $('#tracklist-section').empty();
   $('#tracklist-head').css('display', 'block');
 
   for (var i = 0; i < tracks.length; i++) {
-
     var mediumAlbumArt = tracks[i].image[1]["#text"];
-
     if (mediumAlbumArt.length > 2) { // album art received
       mediumAlbumArt = '<img src=' + mediumAlbumArt + ' />';
     } else { // album art is missing
       mediumAlbumArt = '';
     }
 
-    var listenersNumber = numeral(tracks[i].listeners).format('0,0');
-    var artistData = tracks[i].artist.split(" ").join("");
-
+    var listenersNumber = numeral(tracks[i].listeners).format('0,0'); // format listeners numbers with commas every three decimal places from the left
     var trackList =
-
       '<div class="row">' +
         '<ul class="container" id="track-info">' +
           '<li class="col-xs-1"><a target="_blank" href=' + tracks[i].url + '><i class="fa fa-play-circle-o fa-2x"></i></a></li>' +
           '<li class="col-xs-5">' + (i + 1) + '. ' + tracks[i].name + '</li>' +
-          '<li class="col-xs-2 artist" data-artist=' + artistData +'>' + tracks[i].artist + '</li>' +
+          '<li class="col-xs-2 artist">' + tracks[i].artist + '</li>' +
           '<li class="col-xs-2">' + mediumAlbumArt + '</li>' +
           '<li class="col-xs-2">' + listenersNumber + '</li>' +
         '</ul>' +
       '</div>';
 
-    $('#tracklist-section').append(trackList);
+    $('#tracklist-section').append(trackList); // append 'trackList' elements to the HTML
   }
 };
 
@@ -80,55 +75,61 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title, sort) {
-
-  if (title) { // input isn't empty
-    $('h1').addClass('app-title'); // animate page title
-    $.ajax({
-      url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
-      success: function(response) {
-
-        var responseTrack = response.results.trackmatches.track;
-
-        if (sort != undefined) {  // called from the list header
-          /*
-          https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-          */
-          var sorted = responseTrack.sort(function(x, y) {
-            var array = [y, x];
-            if (isNaN(x[sort])) { // sorting for #Song or Artist
-              if (x[sort].toUpperCase() > y[sort].toUpperCase()) {
-                return 1;
-              }
-              if (x[sort].toUpperCase() < y[sort].toUpperCase()) {
-                return -1;
-              }
-              return 0;
-            } else { // sorting for Listeners
-              return y[sort] - x[sort];
-            }
-
-          })
-          sortCount[sort] = 1 - sortCount[sort];
-          if (sortCount[sort] == 1) { // reverse sorting order
-            sorted = sorted.reverse();
-          }
-        } else { // new search reset sorting count
-          sortCount = {
-            name: 1,
-            artist: 1,
-            listeners: 1
-          };
-          var sorted = responseTrack;
+  if (sort != undefined) { // call sorting
+    Trackster.sortTracks(sort);
+  } else if (title == '') { // empty input
+    Trackster.renderMessage();
+  } else {
+    if (title) { // input isn't empty
+      sortCount = {
+        name: 1,
+        artist: 1,
+        listeners: 1
+      };
+      $('h1').addClass('app-title'); // animate page title
+      $.ajax({
+        url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
+        success: function(response) {
+          responseTrack = response.results.trackmatches.track;
+          Trackster.renderTracks(responseTrack);
+          $('h1').removeClass('app-title'); // remove animation after the list is rendered
         }
+      });
 
-        Trackster.renderTracks(sorted);
-        $('h1').removeClass('app-title'); // remove animation after the list is rendered
-      }
-    });
-  } else { // empty input
-    $('#tracklist-head').css('display', 'none');
-    $('#tracklist-section').empty();
-    var emptyQuery = '<p>Type something in the search query</p>'
-    $('#tracklist-section').append(emptyQuery);
+    } else { // empty input
+      Trackster.renderMessage();
+    }
   }
 };
+
+Trackster.renderMessage = function() {
+  $('#tracklist-head').css('display', 'none');
+  $('#tracklist-section').empty();
+  var emptyQuery = '<p>Type something in the search query</p>'
+  $('#tracklist-section').append(emptyQuery);
+}
+
+/*
+  Sort the data stored in the object 'responseTrack' and call the 'renderTracks' function to render the sorted list
+*/
+Trackster.sortTracks = function(sort) {
+  responseTrack = responseTrack.sort(function(x, y) {
+    if (isNaN(x[sort])) { // sorting for #Song or Artist
+      if (x[sort].toUpperCase() > y[sort].toUpperCase()) {
+        return 1;
+      }
+      if (x[sort].toUpperCase() < y[sort].toUpperCase()) {
+        return -1;
+      }
+      return 0;
+    } else { // sorting for Listeners
+      return y[sort] - x[sort];
+    }
+  })
+
+  sortCount[sort] = 1 - sortCount[sort]; // toggles between 0 and 1
+  if (sortCount[sort] == 1) { // reverse sorting order
+    responseTrack = responseTrack.reverse();
+  }
+  Trackster.renderTracks(responseTrack);
+}
